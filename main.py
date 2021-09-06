d3195672ccbcdf36dc0f574bc4ff35f4b512525b
Sergey <37954897+klevtcov@users.noreply.github.com>
2021-09-06 22:32:20 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print (15 * 20);
+# print (15 * 20);
+#
+# a = 3
+# name = input('Whats your name?')
+# print ('hello, ' , name)
+
+# a = int(input('Enter number'))
+# b = int(input('Enter number'))
+# print (a * b)
+
+
