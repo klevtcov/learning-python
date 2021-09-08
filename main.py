94ae8fca02f276b16ea72f2c472cb0f1efe47867
Sergey <37954897+klevtcov@users.noreply.github.com>
2021-09-08 21:43:14 +0300

    conditions

--- main.py
+++ main.py
@@ -8,8 +8,66 @@
 # b = int(input('Enter number'))
 # print (a * b)
 
-#a = int(input())
-#print(a > 0)
 
-a = int(input())
-print(10 <= a < 100)
+# Boolean
+# a = int(input())
+# print(a > 0)
+
+# a = int(input())
+# print(10 <= a < 100)
+
+# x1, x2, x3 = False, True, False
+# print(not x1 or x2 and x3) -> True
+# # not -> and -> or
+# True or x2 and x3
+# True or False
+# True 
+
+# ((a and b) or ((not a) and (not b)))
+
+# x = 5
+# y = 10
+# y > x * x or y >= 2 * x and x < y
+
+# a = True
+# b = False
+# a and b or not a and not b
+# False
+
+# x = int(input())
+# if x % 2 == 0:
+#     print('Четное')
+# else: 
+#     print('Нечетное')
+
+# a = int(input())
+# b = int(input())
+# if b != 0:
+#     print (a / b)
+# else:
+#     print('Деление не возможно')
+#     b = int(input('Введите ненулевое значение'))
+#     if b == 0:
+#         print('Вы не справились')
+#     else:
+#         print (a / b)
+
+# A = int(input())
+# B = int(input())
+# H = int(input())
+# if H < A:
+#     print('Недосып')
+# elif H > B:
+#     print('Пересып')
+# else:
+#     print('Это нормально')
+
+# lower, upper, x = (int(input()) for _ in range(3))
+# print("Недосып" if x < lower else "Пересып" if x > upper else "Это нормально")
+
+# year = int(input())
+# if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
+#     print('Високосный')
+# else:
+#     print('Обычный')
+
