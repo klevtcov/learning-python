a075360a081f2f2eba6712f0a01e03ee688b8b03
Sergey <37954897+klevtcov@users.noreply.github.com>
2021-10-09 21:21:43 +0300

    files_task1

--- main.py
+++ main.py
@@ -1076,3 +1076,82 @@
 # for i in a:
 #     print(d[i])
 
+# Работа с файлами
+
+# inf = open('file.txt', 'r')  # open('file.txt')
+# s1 = inf.readline()
+# s2 = inf.readline()
+# inf.close()
+# with open('text.txt') as inf:
+#     s1 = inf.readline()
+#     s2 = inf.readline()
+    
+# # здесь файл уже закрыт
+
+# # s = inf.readline().strip()
+# '\t abc \n'.strip() # abc
+
+# import os
+# os.path.join('.', 'dirname', 'filename.txt')
+# '.\dirname\filename.txt'
+
+# # Построчное чтение из файла
+# with open('input.txt') as inf:
+#     for line in inf:
+#         line = line.strip()
+#         print(line)
+
+# ouf = open('file.txt', 'w')
+# ouf.write('Some text\n')
+# ouf.write(str(25))
+# ouf.close()
+# with open('text.txt', 'w') as ouf:
+#     ouf.write('Some text\n')
+#     ouf.write(str(25))
+    
+# # здесь файл уже закрыт
+
+# На прошлой неделе мы сжимали строки, используя кодирование повторов. Теперь нашей задачей будет 
+# восстановление исходной строки обратно.
+
+# Напишите программу, которая считывает из файла строку, соответствующую тексту, сжатому с помощью 
+# кодирования повторов, и производит обратную операцию, получая исходный текст.
+
+# Запишите полученный текст в файл и прикрепите его, как ответ на это задание.
+# В исходном тексте не встречаются цифры, так что код однозначно интерпретируем.
+
+# Примечание. Это первое задание типа Dataset Quiz. В таких заданиях после нажатия "Start Quiz" у вас 
+# появляется ссылка "download your dataset". Используйте эту ссылку для того, чтобы загрузить файл со 
+# входными данными к себе на компьютер. Запустите вашу программу, используя этот файл в качестве входных 
+# данных. Выходной файл, который при этом у вас получится, надо отправить в качестве ответа на эту 
+# задачу.
+
+# a3b4c2e10b1
+# aaabbbbcceeeeeeeeeeb
+
+with open('dataset_3363_2.txt') as inf:
+    a = str(inf.readline())
+print(a)
+#a = "a3b4c2e10b1"
+letters = []
+numbers = []
+decompressed = ''
+i = 0
+while i < len(a):
+    if a[i] >= 'A':
+        letters += a[i]
+    else:
+        if a[i-1] < 'A':
+            numbers[-1] += a[i]
+        else:
+            numbers += a[i]
+    i += 1
+
+i = 0
+while i < len(letters):
+    decompressed += letters[i] * int(numbers[i])
+    i += 1
+
+with open('out.txt', 'w') as ouf:
+    ouf.write(decompressed)
+print(decompressed)
